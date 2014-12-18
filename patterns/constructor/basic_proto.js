7156ef00cb8dc771df47922535075d08ae90cbac
opensoars <sam_1700@hotmail.com>
2014-12-18 10:46:14 +0100

    New file structure and added comments to basic_proto

--- patterns/constructor/basic_proto.js
+++ patterns/constructor/basic_proto.js
@@ -1,15 +1,28 @@
+/**
+ * Car constructor function
+ * Automatically assigns passed in properties from the o parameter
+ *
+ * @param o {object}  Options
+ */
 function Car(o){
   o = o || {};
 
   for(var key in o)
     this[key] = o[key];
 }
-
+  
+/**
+ * Car instance method getInfoString
+ *
+ * @return {string}  Info about the car
+ */
 Car.prototype.getInfoString = function (){
   return 'A ' + this.color + ' ' + this.brand
     + ' which has driven ' + this.km + ' kilometers';
 };
 
+
+// Call Car constructor to get instance
 var car1 = new Car({
   brand: 'Volvo',
   color: 'red',
